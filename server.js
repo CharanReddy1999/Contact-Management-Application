1570630ce852d92276796cbd4f8f1145c58301d3
Charan Reddy <charanreddy2500@gmail.com>
2023-05-18 13:43:57 -0700

    mongoose setup has done.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 // The dotenv module's config method is called to load environment variables from a .env file in the current directory.
 // These environment variables are then accessible through process.env.
-
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5002;
