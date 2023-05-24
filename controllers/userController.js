bb0acc0610ddd482d62b68eed38e21b0b0045414
Charan Reddy <charanreddy2500@gmail.com>
2023-05-24 14:31:47 -0700

    Verify JWT Token, middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,9 +69,9 @@ const loginUser = asyncHandler(async (req, res) => {
 
 //@desc Current user info
 //@route POST /api/users/current
-//@access public
+//@access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
